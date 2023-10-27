d956794d4c3832b7d3585f88d29708107352e798
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-27 02:09:18 -0300

    add posts page

---
+++ src/app/posts/[id]/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tweet / Twitter",
+};
+
+import { TweetNavbar } from "@/components/navbars/TweetNavbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={roboto.className}>
+      <TweetNavbar />
+      {children}
+    </div>
+  );
+}
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { TweetPost } from "@/components/posts/TweetPost";
+
+import { getOneUser } from "@/utils/users/getOneUser";
+
+import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+
+interface IPostPage {
+  params: Params;
+}
+
+export default async function PostPage({ params }: IPostPage) {
+  const { id } = params;
+
+  const user = await getOneUser(id);
+
+  return <TweetPost user={user} />;
+}
