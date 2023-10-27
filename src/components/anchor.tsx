e2292b2353120c1e265a256b4257f3a7e53c3918
Biran <ibiran@FRSN.UTN.EDU.AR>
2023-10-27 17:36:38 -0300

    add anchor component

---
+++ src/components/anchor.tsx
@@ -0,0 +1,12 @@
+interface IAnchor {
+  href?: string;
+  text: string;
+}
+
+export const Anchor = ({ href, text }: IAnchor) => {
+  return (
+    <a href={href} className="text-[#1D9BF0]">
+      {text}
+    </a>
+  );
+};
