3a6029b8e194712963cdb208ddb8dd4214ad9c4a
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-27 02:07:58 -0300

    add twitter posts components

---
+++ src/components/posts/HomePost.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { CommentButton } from "../buttons/CommentButton";
+import { RetweetButton } from "../buttons/RetweetButton";
+import { LikeButton } from "../buttons/LikeButton";
+import { ShareButton } from "../buttons/ShareButton";
+
+import { Avatar } from "@nextui-org/avatar";
+
+import type { TUser } from "@/types/User";
+import type { TPost } from "@/types/Post";
+
+interface IPost {
+  user: TUser;
+}
+
+export const HomePost = ({ user }: IPost) => {
+  const router = useRouter();
+
+  const handleArticleClick = () => {
+    router.push(`/posts/${user.id}`);
+  };
+
+  return (
+    <article
+      onClick={handleArticleClick}
+      className="w-full flex gap-2 p-4 border-b border-b-[#2F3336] cursor-pointer overflow-hidden"
+    >
+      {/* PROFILE PICTURE CONTAINER */}
+      <div>
+        <Avatar src="/luffy.png" />
+      </div>
+
+      {/* POST CONTENT CONTAINER */}
+      <div className="w-full overflow-hidden">
+        {/* POST HEADER */}
+        <div className="w-full flex gap-2 overflow-hidden">
+          <h2 className="font-bold truncate">{user.name}</h2>
+          <h3 className="text-[#71767B] truncate">@{user.username}</h3>
+        </div>
+
+        {/* POST BODY */}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+          voluptates repellendus esse quisquam amet voluptate quibusdam mollitia
+          at eos nihil laborum ad ducimus, accusantium minus, ullam inventore
+          vitae laboriosam quidem. Quas eveniet repellat sed laborum, dolorem
+          quia optio fugiat pariatur quidem voluptates assumenda dolores
+          cupiditate obcaecati magnam vero eius sunt necessitatibus molestias
+          temporibus similique eum delectus laudantium iusto. Quos, nulla!
+        </p>
+
+        {/* POST FOOTER */}
+        <div className="w-full flex justify-between items-center mt-2 text-[#71767B]">
+          <CommentButton />
+          <RetweetButton />
+          <LikeButton />
+          <ShareButton />
+        </div>
+      </div>
+    </article>
+  );
+};
---
+++ src/components/posts/TweetPost.tsx
@@ -0,0 +1,69 @@
+import { CommentButton } from "../buttons/CommentButton";
+import { RetweetButton } from "../buttons/RetweetButton";
+import { LikeButton } from "../buttons/LikeButton";
+import { ShareButton } from "../buttons/ShareButton";
+
+import { Divider } from "@nextui-org/divider";
+
+import type { TUser } from "@/types/User";
+import type { TPost } from "@/types/Post";
+
+interface ITweerPost {
+  user: TUser;
+}
+
+import { User } from "@nextui-org/user";
+
+export const TweetPost = ({ user }: ITweerPost) => {
+  return (
+    <article className="w-full flex flex-col items-center overflow-hidden">
+      <div className="sm:w-[600px] sm:border-l sm:border-r p-4 sm:border-[#2F3336]">
+        <User
+          name={user.name}
+          description={`@${user.username}`}
+          avatarProps={{
+            src: "/luffy.png",
+            alt: "Luffy",
+            size: "lg",
+          }}
+          className="justify-start"
+        />
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+          voluptates repellendus esse quisquam amet voluptate quibusdam mollitia
+          at eos nihil laborum ad ducimus, accusantium minus, ullam inventore
+          vitae laboriosam quidem. Quas eveniet repellat sed laborum, dolorem
+          quia optio fugiat pariatur quidem voluptates assumenda dolores
+          cupiditate obcaecati magnam vero eius sunt necessitatibus molestias
+          temporibus similique eum delectus laudantium iusto. Quos, nulla!
+        </p>
+
+        <span className="text-[#71767B]">2:27 · 25 oct. 2023</span>
+
+        <Divider className="my-4" />
+
+        {/* REPLIES & LIKES HARDCODED */}
+        <section className="flex gap-4">
+          <h3>
+            2 <span className="text-[#71767B]">Replies</span>
+          </h3>
+          <h3>
+            2 <span className="text-[#71767B]">Likes</span>
+          </h3>
+        </section>
+
+        <Divider className="my-4" />
+
+        <section className="w-full flex items-center justify-evenly text-[#71767B]">
+          <CommentButton />
+          <RetweetButton />
+          <LikeButton />
+          <ShareButton />
+        </section>
+
+        <Divider className="my-4" />
+      </div>
+    </article>
+  );
+};
