fa33fa181a3850e55a240a99d4f116728720e56e
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-27 02:06:14 -0300

    add get one user function

---
+++ src/utils/users/getOneUser.ts
@@ -0,0 +1,12 @@
+import type { TUser } from "@/types/User";
+
+export const getOneUser = async (id: number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    {
+      cache: "no-cache",
+    }
+  );
+  const data: TUser = await response.json();
+  return data;
+};
