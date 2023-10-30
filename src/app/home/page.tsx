73df7436f76256ab68ff93cd8f0d5bebea68b9be
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-29 23:42:49 -0300

    add tweet input

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,13 +5,15 @@ import { getAllUsers } from "@/utils/users/getAllUsers";
 
 import { AddTweetButton } from "@/components/buttons/AddTweetButton";
 import { HomeFooter } from "@/components/footers/HomeFooter";
+import { TweetInput } from "@/components/TweetInput";
 
 export default async function HomePage() {
   const users = await getAllUsers();
 
   return (
     <main id="home">
-      
+      <TweetInput />
+
       {users.map((user: TUser) => (
         <HomePost key={user.id} user={user} />
       ))}
---
+++ src/components/TweetInput.tsx
@@ -0,0 +1,22 @@
+import { Avatar } from "@nextui-org/avatar";
+import { Input } from "@nextui-org/input";
+import { Button } from "./buttons/Button";
+
+export const TweetInput = () => {
+  return (
+    <section className="lg:flex hidden justify-center overflow-hidden">
+      <div className="w-[600px] sm:border-[#2F3336] border-b border-b-[#2F3336] flex flex-col gap-6 p-4 cursor-pointer overflow-hidden">
+        <div className="flex items-center gap-4">
+          <Avatar src="/luffy.png" />
+          <Input placeholder="What's happening?" variant="bordered" size="lg" />
+        </div>
+
+        <div className="w-full flex items-end justify-end">
+          <div className="w-[100px]">
+            <Button text="Tweet" variant="primary" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
