c26245b93505154261e9771c6b6db336c6264f5e
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-29 22:40:56 -0300

    add home footer

---
+++ src/components/footers/HomeFooter.tsx
@@ -0,0 +1,24 @@
+import { HiOutlineMail, HiOutlineUser } from "react-icons/hi";
+import { IoHome, IoNotificationsOutline } from "react-icons/io5";
+
+export const HomeFooter = () => {
+  return (
+    <footer className="lg:hidden w-full flex justify-around py-4 px-6 items-center bg-black border-t border-t-[#2F3336] fixed bottom-0 z-50">
+      <button>
+        <IoHome size="30px" />
+      </button>
+
+      <button>
+        <IoNotificationsOutline size="30px" />
+      </button>
+
+      <button>
+        <HiOutlineMail size="30px" />
+      </button>
+
+      <button>
+        <HiOutlineUser size="30px" />
+      </button>
+    </footer>
+  );
+};
