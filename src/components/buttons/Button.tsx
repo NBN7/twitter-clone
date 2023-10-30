68e5481f625146b8c351234f6917e485d06c9e9a
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-30 01:07:43 -0300

    update default button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -9,18 +9,27 @@ interface IButton {
   border?: boolean;
   logo?: string;
   onClick?: () => void;
+  isDisabled?: boolean;
 }
 
-export const Button = ({ text, border, variant, logo, onClick }: IButton) => {
+export const Button = ({
+  text,
+  border,
+  variant,
+  logo,
+  onClick,
+  isDisabled = false,
+}: IButton) => {
   return (
     <button
+      disabled={isDisabled}
       onClick={onClick}
       className={`w-full flex items-center justify-center gap-2 font-bold rounded-3xl py-2 
       ${variant === BUTTON_VARIANTS.DEFAULT ? "text-sky-500 bg-inherit" : ""}
       ${variant === BUTTON_VARIANTS.PRIMARY ? "text-white bg-sky-500" : ""}
       ${variant === BUTTON_VARIANTS.SECONDARY ? "text-black bg-white" : ""} 
       ${border ? "border border-[#536471]" : ""}
-      hover:opacity-80 transition-all`}
+      hover:opacity-80 transition-all disabled:opacity-40`}
     >
       {logo ? <img src={logo} alt="logo" width="25px"></img> : null}
 
