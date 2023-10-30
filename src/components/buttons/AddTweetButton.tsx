70f53d948e19c6006f20e97f08997c3dc03ba317
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-29 22:40:20 -0300

    update add tweet button

--- src/components/buttons/AddTweetButton.tsx
+++ src/components/buttons/AddTweetButton.tsx
@@ -1,10 +1,8 @@
-""
-
 import { AiOutlinePlus } from "react-icons/ai";
 
 export const AddTweetButton = () => {
   return (
-    <button className="lg:hidden fixed rounded-full bg-sky-500 bottom-0 right-0 p-3 mr-4 mb-4 z-50">
+    <button className="lg:hidden fixed rounded-full bg-sky-500 bottom-0 right-0 p-3 mr-4 mb-20 z-50">
       <AiOutlinePlus size="25px" />
     </button>
   );
