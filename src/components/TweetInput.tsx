0042a855f65f5a8164f8d085c4e53f9e2d79b830
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-30 00:38:17 -0300

    update tweet input

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,5 +1,5 @@
 import { Avatar } from "@nextui-org/avatar";
-import { Input } from "@nextui-org/input";
+import { Textarea } from "@nextui-org/input";
 
 import { Button } from "./buttons/Button";
 
@@ -9,7 +9,12 @@ export const TweetInput = () => {
       <div className="w-[600px] sm:border-[#2F3336] border-b border-b-[#2F3336] flex flex-col gap-6 p-4 cursor-pointer overflow-hidden">
         <div className="flex items-center gap-4">
           <Avatar src="/luffy.png" />
-          <Input placeholder="What's happening?" variant="bordered" size="lg" />
+          <Textarea
+            placeholder="What's happening?"
+            variant="bordered"
+            size="lg"
+            minRows={1}
+          />
         </div>
 
         <div className="w-full flex items-end justify-end">
