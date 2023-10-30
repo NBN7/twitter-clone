7a3c9de0492eaa8b4b4d397e5e6e8f5b248179b3
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-29 23:09:05 -0300

    update home navbar

--- src/components/navbars/HomeNavbar.tsx
+++ src/components/navbars/HomeNavbar.tsx
@@ -7,8 +7,6 @@ import { FiSettings } from "react-icons/fi";
 import Link from "next/link";
 
 export const HomeNavbar = () => {
-  const handleAvatarClick = () => {};
-
   return (
     <nav className="lg:hidden w-full flex justify-between py-4 px-6 items-center backdrop-blur-lg border-b border-b-[#2F3336] sticky top-0 z-50">
       {/* LEFT SIDE OF NAVBAR */}
