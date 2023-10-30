b9ab106138ccec3ba889717a5cddd6aafad46294
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-30 00:00:23 -0300

    update add tweet button

--- src/components/buttons/AddTweetButton.tsx
+++ src/components/buttons/AddTweetButton.tsx
@@ -1,9 +1,58 @@
+"use client";
+
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  ModalContent,
+} from "@nextui-org/modal";
+
 import { AiOutlinePlus } from "react-icons/ai";
+import { Button } from "./Button";
+import { Input } from "@nextui-org/input";
 
 export const AddTweetButton = () => {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
+  const handleClick = () => {
+    onClose();
+  };
+
   return (
-    <button className="lg:hidden fixed rounded-full bg-sky-500 bottom-0 right-0 p-3 mr-4 mb-20 z-50">
-      <AiOutlinePlus size="25px" />
-    </button>
+    <>
+      <button
+        onClick={onOpen}
+        className="lg:hidden fixed rounded-full bg-sky-500 bottom-0 right-0 p-3 mr-4 mb-20 z-50"
+      >
+        <AiOutlinePlus size="25px" />
+      </button>
+
+      <Modal
+        placement="center"
+        scrollBehavior="inside"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          <ModalHeader>Tweet</ModalHeader>
+
+          <ModalBody>
+            <Input
+              placeholder="What's happening?"
+              variant="bordered"
+              size="lg"
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <div className="w-[100px]">
+              <Button onClick={handleClick} text="Tweet" variant="primary" />
+            </div>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
