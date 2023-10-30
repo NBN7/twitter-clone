f430d6d572fb1cc0baac0e612b29eb635de54dcf
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-29 21:11:23 -0300

    add landing footer items constant

---
+++ src/constants/landingFooterItems.ts
@@ -0,0 +1,76 @@
+const year = new Date().getFullYear();
+
+export const LANDING_FOOTER_ITEMS = [
+  {
+    name: "About",
+    url: "https://about.twitter.com/es",
+  },
+  {
+    name: "Help Center",
+    url: "https://help.twitter.com/es",
+  },
+  {
+    name: "Terms of Service",
+    url: "https://twitter.com/tos",
+  },
+  {
+    name: "Privacy Policy",
+    url: "https://twitter.com/privacy",
+  },
+  {
+    name: "Cookie Policy",
+    url: "https://twitter.com/privacy",
+  },
+  {
+    name: "Accesibility",
+    url: "https://help.twitter.com/es/resources/accessibility",
+  },
+  {
+    name: "Ads Info",
+    url: "https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html",
+  },
+  {
+    name: "Blog",
+    url: "https://blog.twitter.com/",
+  },
+  {
+    name: "Status",
+    url: "https://status.twitterstat.us/",
+  },
+  {
+    name: "Careers",
+    url: "https://careers.twitter.com/",
+  },
+  {
+    name: "Brand Resources",
+    url: "https://about.twitter.com/es/company/brand-resources",
+  },
+  {
+    name: "Advertising",
+    url: "https://ads.twitter.com/onboarding/18ce55mzyw9/welcome?ref=gl-tw-tw-twitter-advertise",
+  },
+  {
+    name: "Marketing",
+    url: "https://marketing.twitter.com/es_es",
+  },
+  {
+    name: "Twitter for Business",
+    url: "https://business.twitter.com/",
+  },
+  {
+    name: "Developers",
+    url: "https://developer.twitter.com/en",
+  },
+  {
+    name: "Directory",
+    url: "https://twitter.com/i/directory/profiles",
+  },
+  {
+    name: "Settings",
+    url: "https://twitter.com/settings/account",
+  },
+  {
+    name: `© ${year} UTN FRSN`,
+    url: "https://twitter.com/",
+  },
+];
