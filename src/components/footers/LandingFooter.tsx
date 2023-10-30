39122611c00c371e4cba0d92204d3dfd6c6724c0
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-30 00:03:54 -0300

    fix landing footer error

--- src/components/footers/LandingFooter.tsx
+++ src/components/footers/LandingFooter.tsx
@@ -4,8 +4,13 @@ import { LANDING_FOOTER_ITEMS } from "@/constants/landingFooterItems";
 export const LandingFooter = () => {
   return (
     <footer className="hidden lg:flex flex-wrap items-center justify-center fixed bottom-0 w-full p-4 gap-4 text-sm bg-black overflow-hidden">
-      {LANDING_FOOTER_ITEMS.map((item) => (
-        <Anchor text={item.name} href={item.url} variant="default" />
+      {LANDING_FOOTER_ITEMS.map((item, index) => (
+        <Anchor
+          text={item.name}
+          href={item.url}
+          key={index}
+          variant="default"
+        />
       ))}
     </footer>
   );
