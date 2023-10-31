b26e0f4024e684af8dbe75243ee1ab7071277fd4
Biran <ibiran@frsn.utn.edu.ar>
2023-10-31 16:15:57 -0300

    update landing footer

--- src/components/footers/LandingFooter.tsx
+++ src/components/footers/LandingFooter.tsx
@@ -4,7 +4,7 @@ import { LANDING_FOOTER_ITEMS } from "@/constants/landingFooterItems";
 
 export const LandingFooter = () => {
   return (
-    <footer className="hidden lg:flex flex-wrap items-center justify-center fixed bottom-0 w-full p-4 gap-4 text-sm bg-black overflow-hidden">
+    <footer className="hidden lg:flex flex-wrap items-center justify-center w-full p-4 gap-4 text-sm bg-black overflow-hidden">
       {LANDING_FOOTER_ITEMS.map((item, index) => (
         <Anchor
           text={item.name}
