24c69e658b055816d4d562f3cddd5171f840916d
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-11-01 20:50:33 -0300

    add log out modal

--- src/components/LeftAsideUser.tsx
+++ src/components/LeftAsideUser.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  ModalContent,
+} from "@nextui-org/modal";
 import { User } from "@nextui-org/user";
+
+import { Button } from "./buttons/Button";
 
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 
 export const LeftAsideUser = () => {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
+  const handleClick = () => {
+    onClose();
+  };
+
   return (
     <div className="sticky bottom-0 w-[300px] flex flex-col gap-10 p-4">
-      <div className="flex items-center justify-between cursor-pointer">
+      <div
+        onClick={onOpen}
+        className="flex items-center justify-between cursor-pointer"
+      >
         <User
           name="User Name"
           description="@username"
@@ -18,6 +39,25 @@ export const LeftAsideUser = () => {
 
         <BiDotsHorizontalRounded size="20px" />
       </div>
+
+      <Modal
+        placement="center"
+        scrollBehavior="inside"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          <ModalHeader>Account</ModalHeader>
+
+          <ModalBody></ModalBody>
+
+          <ModalFooter>
+            <div className="w-[100px]">
+              <Button onClick={onClose} text="Log out" variant="default" />
+            </div>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
--- src/components/navbars/HomeNavbar.tsx
+++ src/components/navbars/HomeNavbar.tsx
@@ -1,25 +1,62 @@
 "use client";
 
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  ModalContent,
+} from "@nextui-org/modal";
 import { Avatar } from "@nextui-org/avatar";
-import { BiLogoTwitter } from "react-icons/bi";
 
+import { Button } from "../buttons/Button";
+
+import { BiLogoTwitter } from "react-icons/bi";
 import { FiSettings } from "react-icons/fi";
 
 export const HomeNavbar = () => {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
+  const handleClick = () => {
+    onClose();
+  };
+
   return (
     <nav className="lg:hidden w-full flex justify-between py-4 px-6 items-center backdrop-blur-lg border-b border-b-[#2F3336] sticky top-0 z-50">
-      {/* LEFT SIDE OF NAVBAR */}
-      <Avatar className="cursor-pointer" src="/assets/luffy.png" size="sm" />
-      {/* CENTER OF NAVBAR */}
+      <Avatar
+        onClick={onOpen}
+        className="cursor-pointer"
+        src="/assets/luffy.png"
+        size="sm"
+      />
 
       <button onClick={() => scroll(0, 0)}>
         <BiLogoTwitter size="30px" className="text-[#1EA0F1]" />
       </button>
 
-      {/* RIGHT SIDE OF NAVBAR */}
       <button>
         <FiSettings size="20px" />
       </button>
+
+      <Modal
+        placement="bottom"
+        scrollBehavior="inside"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          <ModalHeader>Account</ModalHeader>
+
+          <ModalBody></ModalBody>
+
+          <ModalFooter>
+            <div className="w-[100px]">
+              <Button onClick={handleClick} text="Log out" variant="default" />
+            </div>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </nav>
   );
 };
