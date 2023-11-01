c7a3a072a6951553dd0b1f1d9d903163a797ee8d
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-11-01 01:34:55 -0300

    update import

--- src/hooks/useIcons.ts
+++ src/hooks/useIcons.ts
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-import { FOOTER_ICONS } from "@/constants/icons";
+import { ICONS } from "@/constants/icons";
 
 export const useIcons = () => {
-  const [activeIcon, setActiveIcon] = useState(FOOTER_ICONS.HOME);
+  const [activeIcon, setActiveIcon] = useState(ICONS.HOME);
 
   const changeActiveIcon = (icon: string) => {
     setActiveIcon(icon);
