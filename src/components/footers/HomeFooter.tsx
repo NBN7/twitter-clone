45d8cbd41abd0550658995ceda1ddec7c97e2f55
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-31 21:32:03 -0300

    update home footer

--- src/components/footers/HomeFooter.tsx
+++ src/components/footers/HomeFooter.tsx
@@ -1,24 +1,31 @@
-import { HiOutlineMail, HiOutlineUser } from "react-icons/hi";
-import { IoHome, IoNotificationsOutline } from "react-icons/io5";
+"use client";
+
+import { useIcons } from "@/hooks/useIcons";
+
+import { FOOTER_ICONS } from "@/constants/icons";
+
+import { HomeIcon } from "../icons/HomeIcon";
+import { NotificationsIcon } from "../icons/NotificationsIcon";
+import { MessagesIcon } from "../MessagesIcon";
+import { ProfileIcon } from "../icons/ProfileIcon";
 
 export const HomeFooter = () => {
+  const { activeIcon, changeActiveIcon } = useIcons();
+
+  const handleHomeClick = () => {
+    changeActiveIcon(FOOTER_ICONS.HOME);
+  };
+
+  const handleProfileClick = () => {
+    changeActiveIcon(FOOTER_ICONS.PROFILE);
+  };
+
   return (
     <footer className="lg:hidden w-full flex justify-around py-4 px-6 items-center bg-black border-t border-t-[#2F3336] sticky bottom-0 z-50">
-      <button>
-        <IoHome size="30px" />
-      </button>
-
-      <button>
-        <IoNotificationsOutline size="30px" />
-      </button>
-
-      <button>
-        <HiOutlineMail size="30px" />
-      </button>
-
-      <button>
-        <HiOutlineUser size="30px" />
-      </button>
+      <HomeIcon activeIcon={activeIcon} onClick={handleHomeClick} />
+      <NotificationsIcon />
+      <MessagesIcon />
+      <ProfileIcon activeIcon={activeIcon} onClick={handleProfileClick} />
     </footer>
   );
 };
