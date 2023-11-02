90d75541354de6f67e9b7051e8403de783885693
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-11-02 00:13:31 -0300

    update text area with id

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -6,7 +6,7 @@ import { Button } from "./buttons/Button";
 export const TweetInput = () => {
   return (
     <section className="lg:flex hidden justify-center overflow-hidden">
-      <div className="w-[600px] sm:border-[#2F3336] border-b border-b-[#2F3336] flex flex-col gap-6 p-4 cursor-pointer overflow-hidden">
+      <div className="w-[600px] sm:border-[#2F3336] border-b border-b-[#2F3336] flex flex-col gap-6 p-4 overflow-hidden">
         <div className="flex items-center gap-4">
           <div>
             <Avatar src="/assets/luffy.png" />
--- src/components/asides/RightAside.tsx
+++ src/components/asides/RightAside.tsx
@@ -5,12 +5,15 @@ import { AiOutlineSearch } from "react-icons/ai";
 export const RightAside = () => {
   return (
     <aside className="lg:block hidden w-full min-h-screen border-l border-l-[#2F3336]">
-      <div className="sticky top-0 w-[300px] flex flex-col gap-10 p-4">
+      <div className="sticky top-0 max-w-xs flex flex-col gap-10 p-4">
         <Input
+          id="search"
           variant="flat"
           startContent={<AiOutlineSearch size="20px" />}
           radius="full"
           placeholder="Search Twitter"
+          label=""
+          aria-labelledby="search"
         />
       </div>
     </aside>
